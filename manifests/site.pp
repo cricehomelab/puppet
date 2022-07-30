72e8e87055ce9e0f0f6aa374cfc41dbd31c36cc0
Charlie Rice <charles.e.riceiii@gmail.com>
2022-07-30 17:12:48 -0400

    adding centos node

--- manifests/site.pp
+++ manifests/site.pp
@@ -10,6 +10,10 @@ node "puppet-agent-ubuntu-2.example.com" {
     include apache
     include unattendedupgrades
 }
+# creating a centos node to test my apache module
+node "puppet-agent-centos.example.com" {
+    include apache
+}
 # this was an experiment to get a base set of things on the first check in. 
 node "default" {
     include sysadmins
