d24847aef40b4dfebd87484a77f7916f0c13fd28
Charlie Rice <charles.e.riceiii@gmail.com>
2022-07-30 11:51:40 -0400

    more fun with variables

--- modules/sysadmins/manifests/init.pp
+++ modules/sysadmins/manifests/init.pp
@@ -1,26 +1,24 @@
 # this was an excersize in creating a group called 'sysadmins' and adding users to the group with a specific uid.
 class sysadmins {
+    $group = 'sysadmins'              # creating a variable just to see how this works. 
     # group i want to have created. 
-    $group = 'sysadmins'
-    group { $group:
+    group { $group:                   # the created variable is now the name of the group
         ensure => present,
     }
     # users i want to add on nodes.
     user { 'bob':
         ensure => present,
         uid    => '9999',
-        groups => 'sysadmins',  # Implied Dependancy for sysadmins group
+        groups => 'sysadmins',       # Implied Dependancy for sysadmins group
     }
-
     user { 'nancy':
         ensure => present,
         uid    => '9998',
-        groups => 'sysadmins',
+        groups => $group,            # can use the variable here.
     }
-
     user { 'steve':
         ensure => present,
         uid    => '9997',
-        groups => 'sysadmins',
+        groups => $group,            # can use the variable here.
     }
 }
