59b01865df6da643c4bfb29dd13e98d35d406710
Charlie Rice <charles.e.riceiii@gmail.com>
2022-07-30 01:02:44 -0400

    comments mostly

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -4,18 +4,18 @@ class apache {
     package {'webserver':
         ensure => installed,
         name   => apache2,        
-    }
+    } 
     file { '/var/www/html/index.html':
         ensure  => file,
         source  => 'puppet:///modules/apache/index.html',
-        owner   => 'root',
+        owner   => 'root',                    # user (root in this case) has an implied dependancy for this if root did not exist this could not be applied (more important for non root owners...)
         group   => 'root',
         mode    => '0644',
-        replace => false,                 # line means that it will not replace the file if not present but will put it there if its lost.
+        replace => false,                     # line means that it will not replace the file if not present but will put it there if its lost.
         require => Package['webserver'],
     }
     service { 'apache2' :
         ensure  => running,
-        require => Package['webserver'],  # ensures that apache is installed before trying to enforce this. 
+        require => Package['webserver'],      # ensures that apache is installed before trying to enforce this. 
     }
 }
--- modules/sysadmins/manifests/init.pp
+++ modules/sysadmins/manifests/init.pp
@@ -1,12 +1,14 @@
 # this was an excersize in creating a group called 'sysadmins' and adding users to the group with a specific uid.
 class sysadmins {
+    # group i want to have created. 
     group { 'sysadmins':
         ensure => present,
     }
+    # users i want to add on nodes.
     user { 'bob':
         ensure => present,
         uid    => '9999',
-        groups => 'sysadmins',
+        groups => 'sysadmins',  # Implied Dependancy for sysadmins group
     }
 
     user { 'nancy':
