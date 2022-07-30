cf925d9d115ad98bb374a85ce2a42e5c79d3e060
Charlie Rice <charles.e.riceiii@gmail.com>
2022-07-30 11:27:37 -0400

    updating apache configuration

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -5,17 +5,25 @@ class apache {
         ensure => installed,
         name   => apache2,        
     } 
+    # this is the default page serviced by apache. This is different than the original. 
     file { '/var/www/html/index.html':
         ensure  => file,
         source  => 'puppet:///modules/apache/index.html',
-        owner   => 'root',                    # user (root in this case) has an implied dependancy for this if root did not exist this could not be applied (more important for non root owners...)
-        group   => 'root',
+        owner   => 'root',                    # user (root in this case) has an implied dependancy for this if root did not exist this could not be applied 
+        group   => 'root',                    # (more important for non root owners...)
         mode    => '0644',
-        replace => false,                     # line means that it will not replace the file if not present but will put it there if its lost.
+        replace => false,                     # This line means that it will not replace the file if not present but will put it there if its lost.
+        require => Package['webserver'],      # ensures that apache is installed before trying to enforce this. 
+    }
+    file { '/etc/apache2/apache2.conf':
+        ensure  => file,
+        source  => 'puppet:///modules/apache/apache2base.conf',
         require => Package['webserver'],
     }
     service { 'apache2' :
-        ensure  => running,
-        require => Package['webserver'],      # ensures that apache is installed before trying to enforce this. 
+        ensure    => running,
+        enable    => true,      
+        subscribe => File['/etc/apache2/apache2.conf'],   # Like the require keyword with the addition that the service will restart if the file is changed. 
     }
+    
 }
