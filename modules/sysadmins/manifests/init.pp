e835ff18da8e359c1c4cb9722625835de7da4ba3
Charlie Rice <charles.e.riceiii@gmail.com>
2022-08-01 19:35:19 -0400

    commenting out where i think a problem is...

--- modules/sysadmins/manifests/init.pp
+++ modules/sysadmins/manifests/init.pp
@@ -37,13 +37,13 @@ class sysadmins {
             ensure     => present,
             managehome => true,   
         }
-
+        /*
         file { "home/${u}/.bashrc":
             ensure  => file,
             owner   => $u,
             group   => $u,
             content => 'export PATH=$PATH:/opt/puppetlabs/puppet/bin',
-        }
+        }*/
     }
     /*
     # old way of doing this 
